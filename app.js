cf68985327016e0492bffbbec37d58e7dd28bf9b
Jaybmuhire1 <68017634+Jaybmuhire1@users.noreply.github.com>
2020-08-26 17:13:15 +0200

    Simple Modication

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyCfNT7iw4odJ44w_nr6tAIdbpP8ufENebU",
+    authDomain: "todo-list-8791e.firebaseapp.com",
+    databaseURL: "https://todo-list-8791e.firebaseio.com",
+    projectId: "todo-list-8791e",
+    storageBucket: "todo-list-8791e.appspot.com",
+    messagingSenderId: "880128655005",
+    appId: "1:880128655005:web:fa85d03bb1285308901e74"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var db=firebase.firestore();
+ 
+
+
+
 // Selectors
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
@@ -7,18 +24,33 @@ const filterOption = document.querySelector(".filter-todo");
 
 //EventListeners
 todoButton.addEventListener("click",(e)=>addTodo(e));
+todoInput.addEventListener("input",(e)=>toggleButton(e));
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener('click', filterTodo);
 
 //Functions
+todoButton.disabled = true;
+function toggleButton(e){
+    if (todoInput.value.length == 0) {
+       todoButton.disabled = true;
+    } else {
+        todoButton.disabled = false;
+    } 
+}
+
 function addTodo(event) {
 event.preventDefault();
-    if (todoInput.value.length == 0) {
-        window.alert("Fill this field");
-       
-      } else {
-        const todoDiv = document.createElement("div"); 
-        todoDiv.classList.add("todo");
+//Start of Firebase
+var task = todoInput.value;
+db.collection('List of Todo').add({
+    'task-name':task
+}).then(res=>swal("Task added")).catch(error=>sweetAlert(error));
+//End of Firebase
+
+//alert('Task added')
+
+  const todoDiv = document.createElement("div"); 
+  todoDiv.classList.add("todo");
         
         //create new LI 
         
@@ -26,9 +58,7 @@ event.preventDefault();
         newTodo.innerText = todoInput.value;
         newTodo.classList.add("todo-item"); 
         todoDiv.appendChild(newTodo);
-        //Add TODO TO 
         
-        // saveLocalTodos(todoInput.value);
         //check mark button
         const completedButton = document.createElement("button");
         completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -44,7 +74,7 @@ event.preventDefault();
         todoList.appendChild(todoDiv);
         //clear the input
         todoInput.value=""; 
-        }
+        todoButton.disabled = true;
       }
     
    
@@ -69,36 +99,44 @@ function deleteCheck(e) {
 }
 function filterTodo(e) {
     const todos = todoList.childNodes;
+    // console.log(todos);
     todos.forEach(function(todo) {
-        switch (e.target.value) {
+        switch(e.target.value) {
             case "all":
                 todo.style.display = "flex";
             break;
             case "completed":
-                if(todo.classList.contains("completed")) {
+                if (todo.classList.contains("completed")) {
                     todo.style.dispslay = "flex";
                 } else {
                     todo.style.dispaly = "none";
                 }
-                break;
-                case "uncompleted":
-                    if(!todo.classList.contains("completed")) {
-                        todo.style.dispslay = "flex";
-                    } else {
-                        todo.style.dispaly = "none";
-                    }
-                break;
         }
     });
 }
-// function saveLocalTodos(todos){
-//     check --HEY Do you already have thing in there?
-//     let todos;
-//     if (localStorage.getItem('todos') === null) {
-//         todos =[];
-//     }else{
-//         todos =JSON.parse(localStorage.getItem('todos'));
-//     }
-//     todos.push(todo);
-//     localStorage.setItem("todos", JSON.stringify(todos));
+
+
+
+//Start of firebase code
+// document.getElementById('todolist-form'). addEventListener('submit', saveList);
+// function saveList(e) {
+// 	e.preventDefault();
+//     var list=document.getElementById('item').value;
+//     console.log('hi');
+
+// 	saveName(list);
+// 	// console.log(myInput('subject'));
 // }
+// function saveName(a) {
+// db.collection('List of Todo').doc().set({
+// 	list:a,
+	
+// })
+
+// .then(function (){console.log('Contact Saved');})
+// .catch(function (error){console.log('Failed Contact');})
+// }
+
+
+// End of start of contact form 
+
